f57cdb4ad208fc5c552576500a286aa35184b8ff
Esad Tekin <87081552+esadtekinc@users.noreply.github.com>
2023-01-24 22:02:03 +0300

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://dall-e-p75h.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -45,7 +45,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/posts", {
+        const response = await fetch("https://dall-e-p75h.onrender.com/api/v1/posts", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
